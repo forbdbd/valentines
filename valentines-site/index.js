9ee79a1b8af25132e6e0b94d0c954b6a79fdb7a8
forbdbd <78776646+forbdbd@users.noreply.github.com>
2021-02-08 23:17:23 -0500

    Add files via upload

---
+++ valentines-site/index.js
@@ -0,0 +1,209 @@
+
+function show_more1() {
+    var d = new Date();
+    var n = d.getDate();
+
+    if (n >= 10) {
+        document.getElementById("day1header").style.display = "block";
+        document.getElementById("day1text").style.display = "block";
+        document.getElementById("day1img").style.display = "block";
+        document.getElementById("centerme").style.display = "none";
+
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    else {
+        document.getElementById("centerme").style.display = "block";
+
+    }
+    
+}
+
+function show_more2() {
+    var d = new Date();
+    var n = d.getDate();
+
+    if (n >= 11) {
+        document.getElementById("day2header").style.display = "block";
+        document.getElementById("day2text").style.display = "block";
+        document.getElementById("day2img").style.display = "block";
+        document.getElementById("centerme").style.display = "none";
+
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+
+    }
+    else {
+        document.getElementById("centerme").style.display = "block";
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    
+}
+
+function show_more3() {
+    var d = new Date();
+    var n = d.getDate();
+
+    if (n >= 12) {
+        document.getElementById("day3header").style.display = "block";
+        document.getElementById("day3text").style.display = "block";
+        document.getElementById("day3img").style.display = "block";
+        document.getElementById("centerme").style.display = "none";
+
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    else {
+        document.getElementById("centerme").style.display = "block";
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    
+}
+
+function show_more4() {
+    var d = new Date();
+    var n = d.getDate();
+
+    if (n >= 13) {
+        document.getElementById("day4header").style.display = "block";
+        document.getElementById("day4text").style.display = "block";
+        document.getElementById("day4img").style.display = "block";
+        document.getElementById("centerme").style.display = "none";
+
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    else {
+        document.getElementById("centerme").style.display = "block";
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    
+}
+
+function show_more5() {
+    var d = new Date();
+    var n = d.getDate();
+
+    if (n >= 14) {
+        document.getElementById("day5header").style.display = "block";
+        document.getElementById("day5text").style.display = "block";
+        document.getElementById("day5img").style.display = "block";
+        document.getElementById("centerme").style.display = "none";
+
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+    }
+    else {
+        document.getElementById("centerme").style.display = "block";
+        document.getElementById("day1header").style.display = "none";
+        document.getElementById("day1text").style.display = "none";
+        document.getElementById("day1img").style.display = "none";
+        document.getElementById("day2header").style.display = "none";
+        document.getElementById("day2text").style.display = "none";
+        document.getElementById("day2img").style.display = "none";
+        document.getElementById("day3header").style.display = "none";
+        document.getElementById("day3text").style.display = "none";
+        document.getElementById("day3img").style.display = "none";
+        document.getElementById("day4header").style.display = "none";
+        document.getElementById("day4text").style.display = "none";
+        document.getElementById("day4img").style.display = "none";
+        document.getElementById("day5header").style.display = "none";
+        document.getElementById("day5text").style.display = "none";
+        document.getElementById("day5img").style.display = "none";
+    }
+    
+}
+
+
